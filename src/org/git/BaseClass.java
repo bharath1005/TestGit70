853b45e4de5e243d21a4c2092a58d673f5d72f56
bharath1005 <bharatharuncse@gmail.com>
2018-10-24 15:30:51 +0530

    added subtraction codes

--- src/org/git/BaseClass.java
+++ src/org/git/BaseClass.java
@@ -7,5 +7,7 @@ public class BaseClass {
     public static void add(){
     	System.out.println("This is the NEw Addition Method");
     }
-
+    public static void sub() {
+    	System.out.println("This is NEw Subtraction Method");
+    }
 }
