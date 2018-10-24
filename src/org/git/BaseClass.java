9db0fd3206fc069e3ddc9c8a161f2e3f934077e8
bharath1005 <bharatharuncse@gmail.com>
2018-10-24 14:34:52 +0530

    Added some methods

--- src/org/git/BaseClass.java
+++ src/org/git/BaseClass.java
@@ -4,5 +4,8 @@ public class BaseClass {
 	public static void main(String[] args) {
 		System.out.println("This is the Base class ");
 	}
+    public static void add(){
+    	System.out.println("This is the NEw Addition Method");
+    }
 
 }
